4f0562b31f75fbbf7e0dfbd4c09df2e99b2aa0d5
luminosity <agerio100@naver.com>
2022-03-15 21:45:49 +0900

    signup.php에서 client-side 검증 파일 분리
    client-side에서 검증하는 부분에 대한 JS 파일의 양이 늘어남에 따라 관리의 편리함을 위하여 따로 signupValidation.js 란 파일로 분리하고 링크를 걸었음.

--- signup/signup.php
+++ signup/signup.php
@@ -152,55 +152,8 @@
         </div>
     </form>
 
-    <script>
-        const signupForm = document.querySelector("#signup-form");
-        const signupButton = document.querySelector("#signup-button");
-        const id = document.querySelector("#id");
-        const nickname = document.querySelector("#nickname");
-        const email = document.querySelector("#email");
-        const password = document.querySelector("#password");
-        const passwordCheck = document.querySelector("#password-check");
-
-        signupButton.addEventListener("click", function (e) {
-
-            if (id.getAttribute('data-isValidated') === "true" && email.getAttribute('data-isValidated') === "true") {
-                if (nickname.value === "") {
-                    Swal.fire({
-                        icon: 'error',
-                        title: '회원가입 오류',
-                        text: '닉네임을 입력해 주세요!'
-                    })
-                } else {
-                    if (password.value && password.value === passwordCheck.value) {
-                        if(password.value.length < 8) {
-                            Swal.fire({
-                                icon: 'error',
-                                title: '회원가입 오류',
-                                text: '비밀번호 안전도 미달',
-                                footer: '최소 8자리 이상의 비밀번호를 설정해주세요.'
-                            })
-                        } else {
-                            signupForm.submit();
-                        }
-                    } else {
-                        Swal.fire({
-                            icon: 'error',
-                            title: '회원가입 오류',
-                            text: '비밀번호 불일치',
-                            footer: '비밀번호와 그 확인이 서로 일치해야 합니다. 제대로 입력하셨는지 확인해주세요.'
-                        })
-                    }
-                }
-            } else {
-                Swal.fire({
-                    icon: 'error',
-                    title: '회원가입 오류',
-                    text: '검증 부재',
-                    footer: '아이디와 이메일 검증을 실시해 주세요!'
-                })
-            }
-        });
-    </script>
+    <script src="./signupValidation.js"></script>
+    
 </body>
 
 </html>
